2fd37873c0e549213f30f026ea5f9f5eb351e649
Boris LECLERE <Borisleclere@gmail.com>
2023-12-27 23:10:13 +0100

    Remove logging json reward results

--- tools/withdraw.reward.sh
+++ tools/withdraw.reward.sh
@@ -8,14 +8,34 @@ fi
 
 reward=$(aioznode reward balance --endpoint "http://aioznode:1317")
 
-echo "Reward balance json : $reward"
+# echo "Reward balance json : $reward"
 
-availableBalance=$(echo $reward | jq '.balance[0].amount' | sed 's/\"//g')
-availableBalanceDenom=$(echo $reward | jq '.balance[0].denom' | sed 's/\"//g')
+# Retrieve storage earned
+balance=$(echo $reward | jq '.balance[0].amount' | sed 's/\"//g')
+balanceDenom=$(echo $reward | jq '.balance[0].denom' | sed 's/\"//g')
 
-echo "Available balance to withdraw : $availableBalance $availableBalanceDenom"
+# Retrieve storage earned
+storageEarned=$(echo $reward | jq '.storage_earned[0].amount' | sed 's/\"//g')
+storageEarnedDenom=$(echo $reward | jq '.storage_earned[0].denom' | sed 's/\"//g')
 
-if [[ "$availableBalance" != "null" && "$availableBalance" -gt $WITHDRAW_MIN_VALUE ]]; then
-    echo "Try to withdraw... aioznode reward withdraw --endpoint "http://aioznode:1317" --address $WITHDRAW_ADDRESS_HEX --amount $availableBalance$availableBalanceDenom --priv-key-file $PRIV_KEY_FILE"
-    aioznode reward withdraw --endpoint "http://aioznode:1317" --address $WITHDRAW_ADDRESS_HEX --amount $availableBalance$availableBalanceDenom --priv-key-file $PRIV_KEY_FILE
+# Retrieve storage earned
+deliveryEarned=$(echo $reward | jq '.delivery_earned[0].amount' | sed 's/\"//g')
+deliveryEarnedDenom=$(echo $reward | jq '.delivery_earned[0].denom' | sed 's/\"//g')
+
+# Retrieve withdraw
+withdraw=$(echo $reward | jq '.withdraw[0].amount' | sed 's/\"//g')
+withdrawDenom=$(echo $reward | jq '.withdraw[0].denom' | sed 's/\"//g')
+
+# Retrieve deliver count
+deliveryCount=$(echo $reward | jq '.delivery_counter' | sed 's/\"//g')
+
+echo "Balance : $balance $balanceDenom"
+echo "Storage earned : $storageEarned $storageEarnedDenom"
+echo "Delivery earned : $deliveryEarned $deliveryEarnedDenom"
+echo "Withdraw : $withdraw $withdrawDenom"
+echo "Delivery count : $deliveryCount $deliveryCountDenom"
+
+if [[ "$balance" != "null" && "$balance" -gt $WITHDRAW_MIN_VALUE ]]; then
+    echo "Try to withdraw... aioznode reward withdraw --endpoint "http://aioznode:1317" --address $WITHDRAW_ADDRESS_HEX --amount $balance$balanceDenom --priv-key-file $PRIV_KEY_FILE"
+    aioznode reward withdraw --endpoint "http://aioznode:1317" --address $WITHDRAW_ADDRESS_HEX --amount $balance$balanceDenom --priv-key-file $PRIV_KEY_FILE
 fi
