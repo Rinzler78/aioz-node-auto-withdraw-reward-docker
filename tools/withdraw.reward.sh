f090916467c14d5522921915d0df34d0ac48e6a7
Boris LECLERE <Borisleclere@gmail.com>
2023-12-28 11:04:53 +0100

    Compute remaining rewards to withdraw

--- tools/withdraw.reward.sh
+++ tools/withdraw.reward.sh
@@ -6,37 +6,57 @@ then
     exit 1
 fi
 
-reward=$(aioznode reward balance --endpoint "http://aioznode:1317")
+toNumber(){
+    if [ "$1" == "null" ]; then
+        echo 0
+        return
+    fi
 
+    echo $1
+}
+
+convertToTokenValue(){
+    echo "scale=5; $1 / $TOKEN_DECIMALS" | bc -l
+}
+
+reward=$(aioznode reward balance --endpoint "http://aioznode:1317")
 # echo "Reward balance json : $reward"
 
 # Retrieve storage earned
-balance=$(echo $reward | jq '.balance[0].amount' | sed 's/\"//g')
-balanceDenom=$(echo $reward | jq '.balance[0].denom' | sed 's/\"//g')
+balance=$(toNumber $(echo $reward | jq '.balance[0].amount' | sed 's/\"//g'))
+# balanceDenom=$(echo $reward | jq '.balance[0].denom' | sed 's/\"//g')
 
 # Retrieve storage earned
-storageEarned=$(echo $reward | jq '.storage_earned[0].amount' | sed 's/\"//g')
-storageEarnedDenom=$(echo $reward | jq '.storage_earned[0].denom' | sed 's/\"//g')
+storageEarned=$(toNumber $(echo $reward | jq '.storage_earned[0].amount' | sed 's/\"//g'))
+# storageEarnedDenom=$(echo $reward | jq '.storage_earned[0].denom' | sed 's/\"//g')
 
 # Retrieve storage earned
-deliveryEarned=$(echo $reward | jq '.delivery_earned[0].amount' | sed 's/\"//g')
-deliveryEarnedDenom=$(echo $reward | jq '.delivery_earned[0].denom' | sed 's/\"//g')
+deliveryEarned=$(toNumber $(echo $reward | jq '.delivery_earned[0].amount' | sed 's/\"//g'))
+# deliveryEarnedDenom=$(echo $reward | jq '.delivery_earned[0].denom' | sed 's/\"//g')
 
 # Retrieve withdraw
-withdraw=$(echo $reward | jq '.withdraw[0].amount' | sed 's/\"//g')
-withdrawDenom=$(echo $reward | jq '.withdraw[0].denom' | sed 's/\"//g')
+withdraw=$(toNumber $(echo $reward | jq '.withdraw[0].amount' | sed 's/\"//g'))
+# withdrawDenom=$(echo $reward | jq '.withdraw[0].denom' | sed 's/\"//g')
 
 # Retrieve deliver count
 deliveryCount=$(echo $reward | jq '.delivery_counter' | sed 's/\"//g')
 
-echo "Balance : $balance $balanceDenom"
-echo "Storage earned : $storageEarned $storageEarnedDenom"
-echo "Delivery earned : $deliveryEarned $deliveryEarnedDenom"
-echo "Withdraw : $withdraw $withdrawDenom"
-echo "Delivery count : $deliveryCount $deliveryCountDenom"
-echo ""
+# Retrieve data file count
+storedSegmentCount=$(ls $NODEDATA_SEGMENT_FOLDER | wc -l)
 
-if [[ "$balance" != "null" && "$balance" -gt $WITHDRAW_MIN_VALUE ]]; then
+echo "Node rewards status : "
+echo "=> Balance : $balance $TOKEN_DENOM => $(convertToTokenValue $balance) $TOKEN_NAME"
+echo "=> Storage : $storageEarned $TOKEN_DENOM => $(convertToTokenValue $storageEarned) $TOKEN_NAME"
+echo "=> Delivery : $deliveryEarned $TOKEN_DENOM => $(convertToTokenValue $deliveryEarned) $TOKEN_NAME"
+echo "=> Withdraw : $withdraw $TOKEN_DENOM => $(convertToTokenValue $withdraw) $TOKEN_NAME"
+echo "=> Delivery count : $deliveryCount"
+echo "=> Stored segment count : $storedSegmentCount. Size : $(du -sh $NODEDATA_SEGMENT_FOLDER)"
+minimumBalanceToWithDraw=$(echo "$WITHDRAW_MIN_VALUE" | bc -l)
+remainingBalanceToWithDraw=$(($minimumBalanceToWithDraw - $balance))
+echo "=> Remaining balance to withdraw : $remainingBalanceToWithDraw $TOKEN_DENOM => => $(convertToTokenValue $remainingBalanceToWithDraw) $TOKEN_NAME"
+echo " "
+
+if [ "$balance" -gt "$minimumBalanceToWithDraw" ]; then
     echo "Try to withdraw... aioznode reward withdraw --endpoint "http://aioznode:1317" --address $WITHDRAW_ADDRESS_HEX --amount $balance$balanceDenom --priv-key-file $PRIV_KEY_FILE"
     aioznode reward withdraw --endpoint "http://aioznode:1317" --address $WITHDRAW_ADDRESS_HEX --amount $balance$balanceDenom --priv-key-file $PRIV_KEY_FILE
 fi
